18b52cf1767f2a72c79454197c1a6c6e4bf4f520
bulang <bulang@gaoding.com>
2020-06-06 20:07:44 +0800

    init

---
+++ README.md
@@ -0,0 +1,31 @@
+# 文件结构
+- joint_cards_20200606
+ - test_images: 包含测试图片、测试txt以及测试结果图
+ - joint_cards.py: JointCards类
+ - demo.py: 示例文件
+ - 运行：`python3 demo.py --txt xx/test_images/test_1.txt --image xx/test_images/test_1.jpg`
+
+# test case
+- 对于"test_1.txt":
+ - 输出：[[0, 1], [2, 3]]
+- 对于"test_2.txt":
+ - 输出：[[0, 1, 2, 3, 4]]
+- 对于"test_3.txt":
+ - 输出：[[2], [0, 1], [4, 5], [3]]
+- 对于"test_4.txt":
+ - 输出：[[2, 3], [1, 0]]
+- 对于"test_5.txt":
+ - 输出：[[0], [1, 2]]
+- 对于"test_6.txt":
+ - 输出：[[0, 1], [2]]
+- 对于"test_7.txt":
+ - 输出：[[0], [2, 1]]
+- 对于"test_8.txt":
+ - 输出：[[0, 1]]
+- 对于"test_9.txt":
+ - 输出：[[0], [1]]
+- 对于"test_10.txt":
+ - 输出：[[0], [2, 1], [3]]
+- 对于"test_11.txt":
+ - 输出：[[4, 3, 2, 1, 0]]
+
---
+++ demo.py
@@ -0,0 +1,60 @@
+
+# -*- coding: UTF-8 -*-
+import os
+import sys
+import cv2
+import random
+import argparse
+from joint_cards import JointCards
+
+def get_bbox_info(txt_path):
+    f = open(txt_path)
+    lines = f.readlines()
+    bbox_infos = []
+    for i, line in enumerate(lines):
+        line = line.strip().split(",")
+        char = line[0]
+        x1 = float(line[1])
+        y1 = float(line[2])
+        x2 = float(line[3])
+        y2 = float(line[4])
+        x3 = float(line[5])
+        y3 = float(line[6])
+        x4 = float(line[7])
+        y4 = float(line[8])
+        bbox_infos.append([char, x1, y1, x2, y2, x3, y3, x4, y4])
+    return bbox_infos
+
+def visulaize_results(image_path, bbox_infos, box_cluster):
+    image = cv2.imread(image_path)
+    for i in range(len(box_cluster)):
+        cluster_list = box_cluster[i]
+        random_color = (random.randint(0,255),random.randint(0,255),random.randint(0,255))
+        for j in range(len(cluster_list)):
+            bbox_info = bbox_infos[cluster_list[j]]
+            x1, y1, x2, y2, x3, y3, x4, y4 = bbox_info[1:]
+            image = cv2.rectangle(image, (int(x1), int(y1)), (int(x3), int(y3)), random_color, 3)
+    path, base_name = os.path.split(image_path)
+    save_path = os.path.join(path, base_name.split(".jpg")[0]+"_result.jpg")
+    cv2.imwrite(save_path, image)
+
+
+if __name__ == "__main__":
+    """
+    python3 demo.py --txt /home/dell/zhanglimin/code/pipline/dl_modular_pipeline/joint_cards_20200606/test_images/test_1.txt --image /home/dell/zhanglimin/code/pipline/dl_modular_pipeline/joint_cards_20200606/test_images/test_1.jpg
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--txt', type=str, help='path to txt')
+    parser.add_argument('--image', type=str, help='path to image')
+    args = parser.parse_args()
+    
+    '''1.输入数据格式：[[char, x1, y1, x2, y2, x3, y3, x4, y4],[]...,[]]'''
+    bbox_infos = get_bbox_info(args.txt)
+    '''2.实例化JointCards类'''
+    J = JointCards(bbox_infos)
+    '''3.获取卡片聚类结果'''
+    box_cluster = J.get_results()
+    '''4.输出数据格式：[[0, 1], [2, 3]]'''
+    print(box_cluster)
+    '''5.可视化结果'''
+    visulaize_results(args.image, bbox_infos, box_cluster)
